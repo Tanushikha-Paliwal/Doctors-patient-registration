4c802025e83fb593b5a9c4e7562fddb024562fad
Tanu <tanupaliwal2001@gmail.com>
2023-09-04 18:18:58 +0530

    new update

--- registration/app/admin.py
+++ registration/app/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
+from . models import *
 
 # Register your models here.
+
+admin.site.register(Doctors)
+admin.site.register(Patient)
+admin.site.register(Contact)
 
